d8f3cd5c4b9804eb63d7e0ed7e779036ea23a2aa
win <samuli.siukonen@gmail.com>
2024-10-29 22:14:18 +0200

    add poetry

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,6 +5,9 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        self.varasto2 = Varasto(-1, -1)
+        self.varasto3 = Varasto(2, 1)
+        self.varasto4 = Varasto(1,2)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
@@ -38,3 +41,23 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_negatiivinen(self):
+        alku_saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        loppu_saldo = self.varasto.saldo
+        self.assertAlmostEqual(alku_saldo, loppu_saldo)
+
+    def test_lisays_ei_mahdu(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_negatiivinen(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0)
+
+    def test_ota_kaikki(self):
+        saldo = self.varasto.saldo
+        self.assertAlmostEqual(self.varasto.ota_varastosta(100), saldo)
+
+    def test_tulostus(self):
+        self.assertAlmostEqual(str(self.varasto), "saldo = 0, vielä tilaa 10" )
